4c92cbb8e03179a0965b34206bf7106717a1aef0
Joel Parker Henderson <joel@joelparkerhenderson.com>
2024-03-23 16:14:24 +0000

    Fix options documentation

--- README.md
+++ README.md
@@ -20,19 +20,19 @@ More examples below.
 
 Options for USV separators and modifiers:
 
-* -u, --unit-separator : Set the unit separator string.
+* -u, --us : Set the unit separator (US) string.
 
-* -r, --record-separator : Set the record separator string.
+* -r, --rs : Set the record separator (RS) string.
 
-* -g, --group-separator : Set the group separator string.
+* -g, --gs : Set the group separator (GS) string.
 
-* -f, --file-separator : Set the file separator string.
+* -f, --fs : Set the file separator (FS) string.
 
-* --escape : Set the escape string.
+* -e, --esc : Set the escape (ESC) string.
 
-* --end-of-transmission : Set the end-of-transmission string.
+* -z, --eot : Set the end of transmission (EOT) string.
 
-Options for USV style sets:
+Options for USV style:
 
 * --style-braces : Set the style to use braces, such as "{US}" for Unit Separator.
 
@@ -40,9 +40,21 @@ Options for USV style sets:
 
 * --style-symbols : Set the style to use symbols, such as "␟" for Unit Separator.
 
-* --style-liners : Set the style to use liners wrapping every symbol, such as "\n␟\n" for Unit Separator.
+Options for USV layout:
 
-* --style-sheets : Set the style similar to spreadsheet sheets, such as "␟" for Unit Separator and "␟\n" for Record Separator.
+* --layout-0: Show each item with no line around it. This is no layout, in other words one long line.
+
+* --layout-1: Show each item with one line around it. This is like single-space lines for long form text.
+
+* --layout-2: Show each item with two lines around it. This is like double-space lines for long form text.
+
+* --layout-units: Show each unit on one line. This can be helpful for line-oriented tools.
+
+* --layout-records: Show each record on one line. This is like a typical spreadsheet sheet export.
+
+* --layout-groups: Show each group on one line. This can be helpful for folio-oriented tools.
+
+* --layout-files: Show one file on one line. This can be helpful for archive-oriented tools.
 
 Options for command line tools:
 
@@ -117,14 +129,14 @@ cat example.xlsx | xlsx-to-usv --style-controls
 Output:
 
 ```usv
-Worksheet 1\u001F\u001E
-a\u001Fb\u001F\u001E
-c\u001Fd\u001F\u001E
-\u001D
-Worksheet 2\u001F\u001E
-e\u001Ff\u001F\u001E
-g\u001Fh\u001F\u001E
-\u001D
+Worksheet 1\u{001F}\u{001E}
+a\u{001F}b\u{001F}\u{001E}
+c\u{001F}d\u{001F}\u{001E}
+\u{001D}
+Worksheet 2\u{001F}\u{001E}
+e\u{001F}f\u{001F}\u{001E}
+g\u{001F}h\u{001F}\u{001E}
+\u{001D}
 ```
 
 If you prefer to render markers with braces, to see the markers more easily:
@@ -180,7 +192,7 @@ Constructive feedback welcome. Pull requests and feature requests welcome.
 ## Tracking
 
 * Package: xlsx-to-usv-rust-crate
-* Version: 0.3.0
+* Version: 1.2.0
 * Created: 2024-03-09T13:33:20Z
 * Updated: 2024-03-21T14:05:26Z
 * License: MIT or Apache-2.0 or GPL-2.0 or GPL-3.0 or contact us for more
--- src/main.rs
+++ src/main.rs
@@ -20,19 +20,19 @@
 //! 
 //! Options for USV separators and modifiers:
 //! 
-//! * -u, --unit-separator : Set the unit separator string.
+//! * -u, --us : Set the unit separator (US) string.
 //! 
-//! * -r, --record-separator : Set the record separator string.
+//! * -r, --rs : Set the record separator (RS) string.
 //! 
-//! * -g, --group-separator : Set the group separator string.
+//! * -g, --gs : Set the group separator (GS) string.
 //! 
-//! * -f, --file-separator : Set the file separator string.
+//! * -f, --fs : Set the file separator (FS) string.
 //! 
-//! * --escape : Set the escape string.
+//! * -e, --esc : Set the escape (ESC) string.
 //! 
-//! * --end-of-transmission : Set the end-of-transmission string.
+//! * -z, --eot : Set the end of transmission (EOT) string.
 //! 
-//! Options for USV style sets:
+//! Options for USV style:
 //! 
 //! * --style-braces : Set the style to use braces, such as "{US}" for Unit Separator.
 //! 
@@ -40,9 +40,21 @@
 //! 
 //! * --style-symbols : Set the style to use symbols, such as "␟" for Unit Separator.
 //! 
-//! * --style-liners : Set the style to use liners wrapping every symbol, such as "\n␟\n" for Unit Separator.
+//! Options for USV layout:
 //! 
-//! * --style-sheets : Set the style similar to spreadsheet sheets, such as "␟" for Unit Separator and "␟\n" for Record Separator.
+//! * --layout-0: Show each item with no line around it. This is no layout, in other words one long line.
+//! 
+//! * --layout-1: Show each item with one line around it. This is like single-space lines for long form text.
+//! 
+//! * --layout-2: Show each item with two lines around it. This is like double-space lines for long form text.
+//! 
+//! * --layout-units: Show each unit on one line. This can be helpful for line-oriented tools.
+//! 
+//! * --layout-records: Show each record on one line. This is like a typical spreadsheet sheet export.
+//! 
+//! * --layout-groups: Show each group on one line. This can be helpful for folio-oriented tools.
+//! 
+//! * --layout-files: Show one file on one line. This can be helpful for archive-oriented tools.
 //! 
 //! Options for command line tools:
 //! 
@@ -90,7 +102,7 @@
 //! Run:
 //! 
 //! ```sh
-//! cat example.xlsx | xlsx-to-usv --style-sheets
+//! cat example.xlsx | xlsx-to-usv
 //! ```
 //! 
 //! Output:
@@ -106,7 +118,7 @@
 //! ␝
 //! ```
 //! 
-//! If you prefer ASCII Separated Values (ASV) that use zero-width control characters, then you may style the output with controls. The style braces display the Unit Separator as \u001F, the Record Separator as \u001E, the Group Separator as \u001D.
+//! If you prefer ASCII Separated Values (ASV) with zero-width character controls:
 //! 
 //! Run:
 //! 
@@ -117,7 +129,14 @@
 //! Output:
 //! 
 //! ```usv
-//! Worksheet 1\u001F\u001Ea\u001Fb\u001F\u001Ec\u001Fd\u001F\u001E\u001DWorksheet 2\u001F\u001Ee\u001Ff\u001F\u001Eg\u001Fh\u001F\u001E\u001D
+//! Worksheet 1\u{001F}\u{001E}
+//! a\u{001F}b\u{001F}\u{001E}
+//! c\u{001F}d\u{001F}\u{001E}
+//! \u{001D}
+//! Worksheet 2\u{001F}\u{001E}
+//! e\u{001F}f\u{001F}\u{001E}
+//! g\u{001F}h\u{001F}\u{001E}
+//! \u{001D}
 //! ```
 //! 
 //! If you prefer to render markers with braces, to see the markers more easily:
@@ -129,10 +148,17 @@
 //! Output:
 //! 
 //! ```usv
-//! Worksheet 1{US}{RS}a{US}b{US}{RS}c{US}d{US}{RS}{GS}Worksheet 2{US}{RS}e{US}f{US}{RS}g{US}h{US}{RS}{GS}
+//! Worksheet 1{US}{RS}
+//! a{US}b{US}{RS}
+//! c{US}d{US}{RS}
+//! {GS}
+//! Worksheet 2{US}{RS}
+//! e{US}f{US}{RS}
+//! g{US}h{US}{RS}
+//! {GS}
 //! ```
 //! 
-//! For more, browse the main repo:<br> 
+//! For more, see the official repository:<br> 
 //! [Unicode Separated Values (USV)](https://github.com/sixarm/usv)
 //! 
 //! ## FAQ
@@ -166,7 +192,7 @@
 //! ## Tracking
 //! 
 //! * Package: xlsx-to-usv-rust-crate
-//! * Version: 1.0.0
+//! * Version: 1.2.0
 //! * Created: 2024-03-09T13:33:20Z
 //! * Updated: 2024-03-21T14:05:26Z
 //! * License: MIT or Apache-2.0 or GPL-2.0 or GPL-3.0 or contact us for more
@@ -194,8 +220,8 @@ fn main() -> std::io::Result<()> {
     stdin.read_to_end(&mut buf)?;
     let mut cursor = std::io::Cursor::new(buf);
     match xlsx_to_usv::xlsx_reader_to_usv(cursor, &args.style) {
-        Ok(s) => {
-            println!("{}", &s);
+        Ok(usv) => {
+            println!("{}", &usv);
         },
         Err(e) => {
             error!("{e}");
